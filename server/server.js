79f71d523a7e6177f270a4712a0e1ceaece98612
Viji Vidyadharan <vvidyadharan@hotmail.com>
2018-10-17 21:21:06 -0500

    Emit newMessage on createMessage

--- server/server.js
+++ server/server.js
@@ -14,14 +14,13 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
-    socket.emit('newMessage', {
-        from: 'Viji',
-        text: 'Should we meet at 4 today for tea',
-        createdAt: new Date()
-    })
-
     socket.on('createMessage', (message) => {
         console.log('createMessage', message);
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createAt: new Date().getTime()
+        });
     })
 
     socket.on('disconnect', () => {
