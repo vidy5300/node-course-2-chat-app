103f5e70450b1241b762d0cac441e3ca4b85bd58
Viji Vidyadharan <vvidyadharan@hotmail.com>
2018-10-18 21:38:54 -0500

    Add css for chat page

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ io.on('connection', (socket) => {
         console.log('createMessage', message);
         io.emit('newMessage', generateMessage(message.from,message.text));
 
-        callback('Ths is from the server');
+        callback();
 
         // socket.broadcast.emit('newMessage', {
         //     from: message.from,
